b3ed3ba70298af831b31b9f256ea6f724f2a7140
lucasj <lucasj@MacBook-Pro.local>
2025-03-20 17:49:46 +0800

    to add v2 version of namo

---
+++ crane_core/src/models/namo2.rs
@@ -0,0 +1,6 @@
+// v2 version of Namo-500M-v2
+// it uses Siglip2 as vision encoder and Qwen2.5 500M as llm
+// the connector part is also very simple
+// but we added a VLPatchMerger to siglip2 to reduce tokens
+
+
